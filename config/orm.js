55fe460ac8d352f9ada9a41e6730052f8f70dcfd
dmaddox <dmaddox@gmail.com>
2017-12-21 19:05:10 -0600

    add and update burgers is working

--- config/orm.js
+++ config/orm.js
@@ -1,41 +1,85 @@
 var connection = require("./connection.js");
 
+
+// Helper function for SQL syntax.
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+  var arr = [];
+
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(key + "=" + value);
+    }
+  }
+
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+}
+
+
+
 var orm = {
   // display burgers
-  selectAll: function(selection, table) {
-    var queryString = "SELECT ?? FROM ??;";
-    connection.query(queryString, [selection, table], function(err, result) {
+  all: function(table, cb) {
+    var queryString = "SELECT * FROM " + table + ";";
+    connection.query(queryString, function(err, result) {
         if (err) {
           throw err;
         }
-        console.log(result);
+        
+        cb(result);
     });
   },
   // add a burger
-  insertOne: function(burger) {
-    var queryString = "INSERT INTO burgers (burger_name, devoured) VALUES (?, false)";
-    // var queryString = "INSERT INTO burgers";
-    // queryString += " (burger_name, devoured)";
-    // queryString += "VALUES (";
-    // queryString += burger;
-    // queryString += ", false) ";
+  create: function(table, cols, vals, cb) {
+    // var queryString = "INSERT INTO ?? (cols) VALUES (?, false)";
+    var queryString = "INSERT INTO " + table;
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ")";
+    queryString += " VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ;";
 
     console.log(queryString);
-    connection.query(queryString, [burger], function(err, result, callback) {
+    connection.query(queryString, vals, function(err, result, callback) {
         if (err) {
           throw err;
         }
-        console.log(result);
+        
+        cb(result);
     });
   },
   // change a burger from not eaten to eaten
-  updateOne: function(id) {
-    var queryString = "UPDATE burgers SET devoured = true WHERE id = ?";
-    connection.query(queryString, [id], function(err, result, callback) {
+  update: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table + " SET " + objToSql(objColVals) + " WHERE " + condition + ";";
+    console.log("update: " + queryString);
+    connection.query(queryString, function(err, result, callback) {
         if (err) {
           throw err;
         }
-        console.log(result);
+        
+        cb(result);
     });
   }
 };
--- server.js
+++ server.js
@@ -1,11 +1,24 @@
-var orm = require("./config/orm.js");
+var express = require("express");
+var bodyParser = require("body-parser");
 
-// Console log all the party_name's.
-orm.selectAll("*", "burgers");
+var port = 3000;
 
-// Console log all the client_name's.
-orm.insertOne("delicious");
+var app = express();
 
-// Console log all the parties that have a party-type of grown-up.
-orm.updateOne(6);
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
+app.listen(port);
