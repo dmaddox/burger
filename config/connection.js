eadc1e1537241a0adfc2ded38a2b7667087c992b
dmaddox <dmaddox@gmail.com>
2017-12-22 16:29:28 -0600

    setup if/else statement for Jawsdb on Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+if (process.evn.JAWSDB_URL) {
+	connection = mysql.createConnection(process.evn.JAWSDB_URL)
+} else {
+	connection = mysql.createConnection({
+	  host: "localhost",
+	  user: "root",
+	  password: "",
+	  database: "burgers_db"
+	});
+};
+
 
 connection.connect(function(err) {
   if (err) {
