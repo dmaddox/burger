3ace7d15164b9c2388b1b1970b99701157521d02
dmaddox <dmaddox@gmail.com>
2017-12-22 16:41:06 -0600

    fixed process.env spelling error

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 var mysql = require("mysql");
 var connection;
 
-if (process.evn.JAWSDB_URL) {
-	connection = mysql.createConnection(process.evn.JAWSDB_URL)
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
 	connection = mysql.createConnection({
 	  host: "localhost",
